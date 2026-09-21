9dd8b054b36995226bf1c2e118393b62e0d94e6a
Dona-aa <edonaperaj4@gmail.com>
2026-09-21 19:19:28 +0200

    Add sensor data POST endpoint

--- src/routes/api/sensors/+server.js
+++ src/routes/api/sensors/+server.js
@@ -1,11 +1,30 @@
 import { json } from '@sveltejs/kit';
 
+let latestSensorData = {
+	measuredAt: new Date().toISOString(),
+	temperature: 23.5,
+	humidity: 60,
+	soilMoisture: 45,
+	light: 850
+};
+
 export function GET() {
-	return json({
+	return json(latestSensorData);
+}
+
+export async function POST({ request }) {
+	const data = await request.json();
+
+	latestSensorData = {
 		measuredAt: new Date().toISOString(),
-		temperature: 23.5,
-		humidity: 60,
-		soilMoisture: 45,
-		light: 850
+		temperature: data.temperature,
+		humidity: data.humidity,
+		soilMoisture: data.soilMoisture,
+		light: data.light
+	};
+
+	return json({
+		message: 'Sensordaten wurden empfangen',
+		sensorData: latestSensorData
 	});
 }
